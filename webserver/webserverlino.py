8e63e764b041b9f1de4f35eec3868f437963ee41
linowsmr <luwandino@hotmail.com>
2016-01-02 11:39:15 +0700

    Lino

--- webserver/webserverlino.py
+++ webserver/webserverlino.py
@@ -2,6 +2,21 @@ import threading
 import socket
 import time
 import sys
+import re
+
+html = """ 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webserver | Lino</title>
+</head>
+<body style="background-color: #00c0ff ">
+	<center>
+		<h1 style="color: white"><strong>Webserver</strong></h1>
+		<p style="color: white">Selamat Datang</p>
+	<center>
+</body>
+</html>"""
 
 class MemprosesClient(threading.Thread):
 	def __init__(self,client_socket,client_address,nama):
@@ -13,12 +28,12 @@ class MemprosesClient(threading.Thread):
 	def run(self):
 		message = ''
 		while True:
-			data = self.client_socket.recv(4096)
+			data = self.client_socket.recv(40960)
 			if data:
 				message = message + data #collect seluruh data yang diterima
 				print data
 				if(data):
-					self.client_socket.send('lino')
+					self.client_socket.sendall(html)
 					break	
 			else:
 				break
@@ -27,7 +42,7 @@ class MemprosesClient(threading.Thread):
 class Server(threading.Thread):
 	def __init__(self):
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server_address = ('0.0.0.0',8844)
+		self.server_address = ('localhost',8844)
 		self.my_socket.bind(self.server_address)
 		threading.Thread.__init__(self)
 		print "Webserver berhasil"
